ba7027793178388c725ca79de352eebd3d4de88e
Vargas-Veronica <vargas.veronica55@gmail.com>
2023-09-11 18:40:14 -0300

    Agrego fotos, anclas para marvel.html y dc.html

--- front/js/productos.js
+++ front/js/productos.js
@@ -5,21 +5,63 @@ const Products =
         name: 'Marvel legacy',
         price: '120.000',
         description: 'Marvel legacy',
-        image: '/front/images/marvel1.webp'
+        image: '/front/images/marvel-ironMan.jpg'
     },
     {
         id:2,
         name: 'Fantastic Four',
         price: '150.000',
         description: 'Fantastic Four',
-        image:  '/front/images/fantasticFour.jpg'
+        image:  '/front/images/marvel-avengers.jpg'
     },
     {
         id:3,
         name: 'Superman',
         price: '200.000',
         description: 'Batman',
-        image:  '/front/images/superman.jpg'
+        image:  '/front/images/marvel-capitanAmerica.jpg'
+    },
+    {
+        id:4,
+        name: 'Deadpool',
+        price: '300.000',
+        description: 'Deadpool',
+        image:  '/front/images/marvel-deadpool.jpg'
+    },
+    {
+        id:6,
+        name: 'Spiderman',
+        price: '400.000',
+        description: 'Spiderman',
+        image:  '/front/images/marvel-spiderman.jpg'
+    },
+    {
+        id:6,
+        name: 'Black Widow',
+        price: '300.000',
+        description: 'Black Widow',
+        image:  '/front/images/marvel-blackwidow.jpg'
+    },
+    {
+        id:7,
+        name: 'X-Men',
+        price: '700.000',
+        description: 'X-Men',
+        image:  '/front/images/marvel-xmen.jpg'
+    },
+    {
+        id:8,
+        name: 'Daredevil',
+        price: '600.000',
+        description: 'Daredevil',
+        image:  '/front/images/marvel-daredevil.jpg'
+    },
+    {
+        id:9,
+        name: 'The amazing Spiderman',
+        price: '400.000',
+        description: 'The amazing Spiderman',
+        image:  '/front/images/marvel-spiderman-amazing.jpg'
     },
 ]
 
@@ -34,8 +76,9 @@ const loadEvents = () =>
         button.addEventListener('click', ()=> {
             const selectedProduct = Products.find(product => product.id === Number(button.id))
             if(selectedProduct){
-                alert(`Se agregó al carrito el producto: ${selectedProduct.name}`)
-                cart.push(selectedProduct)
+                window.location.href = '/front/images/fantasticFour.jpg';
+                /*alert(`Se agregó al carrito el producto: ${selectedProduct.name}`)
+                cart.push(selectedProduct)*/
             }
         })    
     }
@@ -46,11 +89,16 @@ const createProducts = () =>
     Products.forEach(product => {
         const card = document.createElement('div')
         card.innerHTML = `
-            <img src='${product.image}'  alt='${product.description}'>           
+            <img src='${product.image}'  alt='${product.description}'> 
+            <div class="card-content">   
             <h3>$${product.price}</h3>
             <h4>${product.description}</h4>
+           
             <button id='${product.id}' class='button'>Agregar al carrito</button>
             </div>
+            </div>
+          
+            
         `
         root.appendChild(card)
     });
