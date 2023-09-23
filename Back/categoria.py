c933ed9b28210fcb6ffd7b22ceeaa62b951d9998
Jeane <jeanette.armoa@gmail.com>
2023-09-23 16:53:59 -0300

    clases back

--- Back/categoria.py
+++ Back/categoria.py
@@ -1,12 +1,12 @@
-class categoria:
+# class categoria:
 
-    def __init__(self, idCategoria, nombreCategoria):
-        self.idCategoria = idCategoria
-        self.nombreCategoria = nombreCategoria
+#     def __init__(self, idCategoria, nombreCategoria):
+#         self.idCategoria = idCategoria
+#         self.nombreCategoria = nombreCategoria
 
-        def getIdCategoria(self):
-            return self.idCategoria
-        def getNombreCategoria(self):
-            return self.nombreCategoria
+#         def getIdCategoria(self):
+#             return self.idCategoria
+#         def getNombreCategoria(self):
+#             return self.nombreCategoria
         
         
