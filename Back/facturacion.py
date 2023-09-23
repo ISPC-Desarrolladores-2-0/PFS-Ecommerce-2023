5ab3da7a04612a2dc2f47596211129cd5575da00
Jeane <jeanette.armoa@gmail.com>
2023-09-23 16:52:08 -0300

    back

---
+++ Back/facturacion.py
@@ -0,0 +1,27 @@
+class facturacion:
+
+    def __init__(self, nombre, apellido, dni, direccion, numCalle, localidad, mp):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.direccion = direccion
+        self.numCalle = numCalle
+        self.localidad = localidad
+        self.mp = mp
+
+        def getNombre(self):
+            return self.nombre
+        def getApellido(self):
+            return self.apellido
+        def getDni(self):
+            return self.dni
+        def getDireccion(self):
+            return self.direccion
+        def getNumCalle(self):
+            return self.numCalle
+        def getLocalidad(self):
+            return self.localidad
+        def getMp(self):
+            return self.mp
+        
+        
