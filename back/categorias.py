f4ef99d54e20bda5d2b3a46f0ab9003bb17fa2d9
nico_153 <nicochurquina11@gmail.com>
2023-09-23 22:17:58 -0300

    Agrego Clase Categoria en el Back

---
+++ back/categorias.py
@@ -0,0 +1,18 @@
+class Categoria():
+
+     def __init__(self, IdCategoria, NombreCategoria):
+         self.idCategoria = IdCategoria
+         self.nombreCategoria = NombreCategoria
+
+         def getIdCategoria(self):
+             return self.idCategoria
+         def getNombreCategoria(self):
+             return self.nombreCategoria
+         def setAgregarComicCategoria(self):
+             return self.agregarComicCategoria
+         def getEliminarComicCategoria(self):
+             return self.eliminarComicCategoria
+         def getFiltrarListaCategoria(self):
+             return self.filtrarListaCategoria
+         def setFiltrarListaCategoria(self):
+             return self.filtrarListaCategoria
