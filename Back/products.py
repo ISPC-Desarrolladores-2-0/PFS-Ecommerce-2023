6292f8cdf57850dff737323bf3eea5a8fec00889
Vargas-Veronica <lucasjaviervillalba@gmail.com>
2023-09-24 14:12:08 -0300

    arreglo products.py

--- Back/products.py
+++ Back/products.py
@@ -1,31 +1,98 @@
-class pedidos:
-    def __init__(self, idProducto, nombre, descripcion, precio, stock, imagen, categoria):
-        self.idProducto = idProducto
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.precio = precio
+class Product:
+    id_products = ""
+    name = ""
+    description = ""
+    price = ""
+    discount = ""
+    stock = ""
+    image = ""
+    pages = ""
+    formato = ""
+    weight = ""
+    isbn = ""
+    id_categories = ""
+
+    def __init__(self, id_products, name, description, price, discount, stock, image, pages, formato, weight, isbn, id_categories):
+        self.id_products = id_products
+        self.name = name
+        self.description = description
+        self.price = price
+        self.discount = discount
         self.stock = stock
-        self.imagen = imagen
-        self.categoria = categoria
+        self.image = image
+        self.pages = pages
+        self.formato = formato
+        self.weight = weight
+        self.id_categories = id_categories
 
-        def getIdProducto(self):
-            return self.idProducto
-        def getNombre(self):
-            return self.nombre
-        def getDescripcion(self):
-            return self.descripcion
-        def getPrecio(self):
-            return self.precio
-        def getStock(self):
-            return self.stock
-        def getImagen(self):
-            return self.imagen
-        def getCategoria(self):
-            return self.categoria
-        
+    def get_id_products(self):
+        return self.id_products
 
-idProducto = input(int("Id Producto"))
-fechaPedido = input ("Fecha de Pedido")
-idUsuario = input (int("Id Usuario:"))
-detalleTotal = input ("Detalle: ")
+    def set_id_products(self, id_products):
+        self.id_products = id_products
 
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_description(self):
+        return self.description
+
+    def set_description(self, description):
+        self.description = description
+
+    def get_price(self):
+        return self.price
+
+    def set_price(self, price):
+        self.price = price
+
+    def get_discount(self):
+        return self.discount_
+
+    def set_discount(self, discount):
+        self.discount = discount
+
+    def get_stock(self):
+        return self.stock
+
+    def set_stock(self, stock):
+        self.stock = stock
+
+    def get_image(self):
+        return self.image
+
+    def set_image(self, image):
+        self.image = image
+
+    def get_pages(self):
+        return self.pages
+
+    def set_pages(self, pages):
+        self.pages = pages
+
+    def get_formato(self):
+        return self.formato
+
+    def set_formato(self, formato):
+        self.formato = formato
+
+    def get_weight(self):
+        return self.weight
+
+    def set_weight(self, weight):
+        self.weight = weight
+
+    def get_isbn(self):
+        return self.isbn
+
+    def set_isbn(self, isbn):
+        self.isbn = isbn
+
+    def get_id_categories(self):
+        return self.id_categories
+
+    def set_id_categories(self, id_categories):
+        self.id_categories = id_categories
