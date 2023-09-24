405dbc5219e0c25b26691220da612e1bdc403879
Jeane <jeanette.armoa@gmail.com>
2023-09-24 20:03:25 -0300

    setters

--- Back/pedidos.py
+++ Back/pedidos.py
@@ -1,18 +1,38 @@
 class pedidos:
+
+    idPedido = 0
+    fechaPedido = 0
+    idUsuario = 0
+    detalleTotal = 0
+
+
     def __init__(self, idPedido, fechaPedido, idUsuario, detalleTotal):
         self.idPedido = idPedido
         self.fechaPedido = fechaPedido
         self.idUsuario = idUsuario
         self.detalleTotal = detalleTotal
 
-        def getIdPedido(self):
+        def getidPedido(self):
             return self.idPedido
-        def getFechaPedido(self):
+        def getfechaPedido(self):
             return self.fechaPedido
-        def getIdUsuario(self):
+        def getidUsuario(self):
             return self.idUsuario
-        def getDetalleTotal(self):
+        def getdetalleTotal(self):
             return self.detalleTotal
+        
+        def setidPedido(self, idPedido):
+            self.idPedido = idPedido
+        
+        def setfechaPedido(self, fechaPedido):
+            self.fechaPedido = fechaPedido
+        
+        def setidUsuario(self, idUsuario):
+            self.idUsuario = idUsuario
+        
+        def setdetalleTotal(self, detalleTotal):
+            self.detalleTotal = detalleTotal
+
         
      
 
