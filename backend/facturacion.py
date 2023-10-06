58c224a5d2cb1c51511c13e6348d94c4a46d692a
Emilce Robles <emi.nrobles@gmail.com>
2023-10-06 18:50:17 -0300

    Se agregan validaciones

--- backend/facturacion.py
+++ backend/facturacion.py
@@ -1,124 +1,4 @@
-"""
-class Facturacion: 
-    id_facturacion = 0
-    id_cliente = 0
-    nombre=""
-    apellido=""
-    dni=""
-    direccion=""
-    numero_calle=""
-    localidad=""
-    metodo_pago=""
-
-    def __init__(self, id_facturacion, id_cliente, nombre, apellido, dni, direccion, numero_calle, localidad, metodo_pago):
-        self.id_facturacion = id_facturacion
-        self.id_cliente = id_cliente
-        self.nombre = nombre
-        self.apellido = apellido
-        self.dni = dni
-        self.direccion = direccion
-        self.numero_calle = numero_calle
-        self.localidad = localidad
-        self.metodo_pago = metodo_pago
-
-    def get_name(self):
-        return self.nombre
-    
-    def get_apellido(self):
-        return self.apellido
-    
-    def get_dni(self):
-        return self.dni
-    
-    def get_direccion(self):
-        return self.direccion
-    
-    def get_numero_calle(self):
-        return self.numero_calle
-    
-    def get_localidad(self):
-        return self.localidad
-    
-    def get_metodo_pago(self):
-        return self.metodo_pago
-    
-    def get_id_facturacion(self):
-        return self.id_facturacion
-    
-    def get_id_cliente(self):
-        return self.id_cliente
-    
-    def set_name(self, nombre):
-        self.nombre = nombre
-    
-    def set_apellido(self, apellido):
-        self.apellido = apellido
-    
-    def set_dni(self, dni):
-        self.dni = dni
-    
-    def set_direccion(self, direccion):
-        self.direccion = direccion
-    
-    def set_numero_calle(self, numero_calle):
-        self.numero_calle = numero_calle
-    
-    def set_localidad(self, localidad):
-        self.localidad = localidad
-    
-    def set_metodo_pago(self, metodo_pago):
-        self.metodo_pago = metodo_pago
-    
-    def set_id_facturacion(self, id_facturacion):
-        self.id_facturacion = id_facturacion
-    
-    def set_id_cliente(self, id_cliente):
-        self.id_cliente = id_cliente
-        
-    def calcular_precio_total(self, precio_producto, cuotas=1):
-        if self.metodo_pago == "Efectivo" or self.metodo_pago == "Transferencia":
-            return precio_producto
-        elif self.metodo_pago == "Tarjeta de Crédito":
-            if cuotas == 1:
-                return precio_producto * 1.10
-            elif cuotas == 3:
-                return precio_producto * 1.20
-            elif cuotas == 6:
-                return precio_producto * 1.30
-            else:
-                raise ValueError("El número de cuotas no es válido")
-        else:
-            raise ValueError("Método de pago no válido")
-
-def mostrar_menu():
-    print("Métodos de Pago:")
-    print("1. Efectivo o Transferencia")
-    print("2. Tarjeta de Crédito (1 cuota)")
-    print("3. Tarjeta de Crédito (3 cuotas)")
-    print("4. Tarjeta de Crédito (6 cuotas)")
-    opcion = int(input("Elija una opción (1-4): "))
-    return opcion
-
-def main():
-    factura = Facturacion(id_facturacion, id_cliente, nombre, apellido, dni, direccion, numero_calle, localidad, metodo_pago)
-    precio_producto = float(input("Ingrese el precio del producto: "))
-
-    opcion = mostrar_menu()
-
-    if opcion == 1:
-        precio_total = factura.calcular_precio_total(precio_producto)
-    elif opcion in [2, 3, 4]:
-        cuotas = opcion  # La opción 2 corresponde a 1 cuota, la 3 a 3 cuotas y la 4 a 6 cuotas.
-        precio_total = factura.calcular_precio_total(precio_producto, cuotas)
-    else:
-        print("Opción inválida. Por favor, seleccione una opción válida.")
-
-    print(f"El precio total es: {precio_total}")
-
-if __name__ == "__main__":
-    main()    
-"""
-
+import re
 
 class Facturacion: 
     id_facturacion = 0
@@ -214,6 +94,43 @@ class Facturacion:
                 raise ValueError("El número de cuotas no es válido")
         else:
             raise ValueError("Método de pago no válido")
+    
+    def validar_numero_tarjeta(self, numero_tarjeta):
+        while not numero_tarjeta.isdigit() or len(numero_tarjeta) != 16:
+            numero_tarjeta = input("El número de tarjeta no es válido. Ingrese nuevamente: ")
+        return numero_tarjeta
+
+    def validar_nombre_titular(self, nombre_titular):
+        while len(nombre_titular.split()) != 2:
+            nombre_titular = input("El nombre del titular debe tener nombre y apellido. Ingrese nuevamente: ")
+        return nombre_titular
+
+    def validar_vencimiento(vencimiento):
+        while True:
+            if not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", vencimiento):
+                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+                continue
+
+            mes, anio = vencimiento.split('/')
+
+            if not (mes.isdigit() and anio.isdigit()):
+                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+                continue
+
+            mes, anio = int(mes), int(anio)
+
+            if not (1 <= mes <= 12 and 0 <= anio <= 99):
+                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+                continue
+
+            return vencimiento
+        
+    
+
+    def validar_codigo_verificacion(self, codigo_verificacion):
+        while not codigo_verificacion.isdigit() or len(codigo_verificacion) != 3:
+            codigo_verificacion = input("El código de verificación no es válido. Ingrese nuevamente: ")
+        return codigo_verificacion
 
 def mostrar_menu():    
     print("\nMétodos de Pago:\n")
@@ -293,12 +210,14 @@ def procesar_factura():
             print(f"El precio con cuotas es: {precio_con_cuotas}")
 
             confirmacion = input("¿Desea continuar con el pago? (s/n): ")
-
+           
             if confirmacion.lower() == "s":
                 numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion = solicitar_datos_tarjeta()
 
-                # Faltan las validaciones para los datos de la tarjeta (número, vencimiento, CVV)
-                
+                numero_tarjeta = factura.validar_numero_tarjeta(numero_tarjeta)
+                nombre_titular = factura.validar_nombre_titular(nombre_titular)
+                vencimiento = factura.validar_vencimiento(vencimiento)
+                codigo_verificacion = factura.validar_codigo_verificacion(codigo_verificacion)                
 
                 print("Datos de tarjeta validados.")
                 confirmacion = input("¿Desea continuar con el pago? (s/n): ")
