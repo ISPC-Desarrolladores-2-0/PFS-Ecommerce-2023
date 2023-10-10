6739ebc23a81eaf824bb800886b49abf8ae35a29
Emilce Robles <emi.nrobles@gmail.com>
2023-10-09 23:19:47 -0300

    Se agregan validaciones para TC y Efectivo

--- backend/facturacion.py
+++ backend/facturacion.py
@@ -95,12 +95,14 @@ class Facturacion:
                 raise ValueError("El número de cuotas no es válido")
         else:
             raise ValueError("Método de pago no válido")
+        
     
     def validar_numero_tarjeta(self, numero_tarjeta):
         while not numero_tarjeta.isdigit() or len(numero_tarjeta) != 16:
             numero_tarjeta = input("El número de tarjeta no es válido. Ingrese nuevamente: ")
         return numero_tarjeta
-
+    
+    
     def validar_nombre_titular(self, nombre_titular):
         while len(nombre_titular.split()) != 2:
             nombre_titular = input("El nombre del titular debe tener nombre y apellido. Ingrese nuevamente: ")
@@ -110,7 +112,7 @@ class Facturacion:
     def validar_vencimiento(vencimiento):
         while True:
             if not re.match(r"^[0-9/]+$", vencimiento):
-                vencimiento = input("El formato no es válido. Por favor, ingrese solo 2 números para el mes, seguido de '/', y luego 2 números para el año: ")
+                vencimiento = input("El formato del vencimiento no es válido. Por favor, ingrese solo 2 números para el mes, seguido de '/', y luego 2 números para el año: ")
             elif not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", vencimiento):
                 vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
             else:
@@ -126,65 +128,98 @@ class Facturacion:
                     vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
                     continue
 
-                # Obtener la fecha actual
                 fecha_actual = datetime.now()
                 fecha_comparar = datetime(fecha_actual.year % 100, fecha_actual.month, 1)
 
                 if fecha_comparar > datetime(anio, mes, 1):
                     vencimiento = input("La fecha ingresada está vencida. Ingrese nuevamente (MM/AA): ")
-                    continue
+                    continue               
+            return vencimiento  
+        
                 
-            return vencimiento    
-        return vencimiento
-        
-        
     def validar_codigo_verificacion(self, codigo_verificacion):
         while not codigo_verificacion.isdigit() or len(codigo_verificacion) != 3:
             codigo_verificacion = input("El código de verificación no es válido. Ingrese nuevamente: ")
         return codigo_verificacion
 
-def mostrar_menu():    
-    print("\nMétodos de Pago:\n")
-    print("1. Efectivo")
-    print("2. Transferencia")
-    print("3. Tarjeta de Débito")
-    print("4. Tarjeta de Crédito") 
-    opcion = int(input("Elija una opción ingresando el N°: "))
-    return opcion
+
+def mostrar_menu():
+    while True:
+        print("\nMétodos de Pago:\n")
+        print("1. Efectivo")
+        print("2. Transferencia")
+        print("3. Tarjeta de Débito")
+        print("4. Tarjeta de Crédito")
+        opcion = input("Elija una opción ingresando el N°: ")
+
+        if opcion in ["1", "2", "3", "4"]:
+            return int(opcion)
+        else:
+            print("Opción inválida. Por favor, seleccione una opción válida.")
+
 
 def solicitar_cuotas():
-    print("\nOpciones de cuotas:")
-    print("1 cuota (5% de recargo)")
-    print("3 cuotas (15% de recargo)")
-    print("6 cuotas (30% de recargo)")
-    opcion_cuotas = int(input("Elija una opción ingresando el N° de cuotas: "))
+    while True:
+        print("\nOpciones de cuotas:")
+        print("1 cuota (5% de recargo)")
+        print("3 cuotas (15% de recargo)")
+        print("6 cuotas (30% de recargo)")
+        opcion_cuotas = input("Elija una opción ingresando el N° de cuotas: ")
 
-    if opcion_cuotas in [1, 3, 6]:
-        return opcion_cuotas
-    else:
-        raise ValueError("El número de cuotas no es válido.")
+        if opcion_cuotas in ["1", "3", "6"]:
+            return int(opcion_cuotas)
+        else:
+            print("El número de cuotas no es válido. Por favor, seleccione una opción válida.")
+
 
 def solicitar_datos_tarjeta():
-    print("\nOpciones de tarjetas:")
-    print("1. VISA")
-    print("2. Mastercard")
-    print("3. Naranja")
-    opcion_tarjeta = int(input("Elija una opción ingresando el N°: "))
+    while True:
+        print("\nOpciones de tarjetas:")
+        print("1. VISA")
+        print("2. Mastercard")
+        print("3. Naranja")
+        opcion_tarjeta = input("Elija una opción ingresando el N°: ")
 
-    if opcion_tarjeta in [1, 2, 3]:
-        numero_tarjeta = input("Ingrese el número de la tarjeta: ")
-        nombre_titular = input("Ingrese el nombre del titular de la tarjeta: ")
-        vencimiento = input("Ingrese el vencimiento de la tarjeta (MM/AA): ")
-        codigo_verificacion = input("Ingrese el código de verificación: ")
+        if opcion_tarjeta in ["1", "2", "3"]:
+            tarjeta_elegida = None
+            if opcion_tarjeta == "1":
+                tarjeta_elegida = "VISA"
+            elif opcion_tarjeta == "2":
+                tarjeta_elegida = "Mastercard"
+            elif opcion_tarjeta == "3":
+                tarjeta_elegida = "Naranja"
 
-        return numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion
+            numero_tarjeta = input("Ingrese el número de la tarjeta: ")
+            nombre_titular = input("Ingrese el nombre del titular de la tarjeta: ")
+            vencimiento = input("Ingrese el vencimiento de la tarjeta (MM/AA): ")
+            codigo_verificacion = input("Ingrese el código de verificación: ")
+            return numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion, tarjeta_elegida
+        else:
+            print("Opción de tarjeta inválida. Por favor, seleccione una opción válida.")
+
+def validar_localidad_cba(localidad):
+    return localidad.lower() in ['cordoba capital', 'córdoba capital']
+
+def pago_en_efectivo(localidad):    
+    if validar_localidad_cba(localidad):
+        print("\nOpciones para pago en efectivo:")
+        print("1. Retirar en el local")
+        print("2. Abonar al recibir en domicilio")
+        opcion_efectivo = input("Elija una opción ingresando el N°: ")
+
+        if opcion_efectivo == "1":
+            print("Le pedimos que se acerque al local en los próximos 5 días habiles. Caso contrario se procederá a contarlo")  # Precio ficticio
+        elif opcion_efectivo == "2":
+            print("En las proximas 24hs habiles se realizará la entrega de su pedido al domicilio que indicó sin costo adicional.")  # Precio ficticio
+        else:
+            print("Opción inválida. Por favor, seleccione una opción válida.")
     else:
-        raise ValueError("Opción de tarjeta inválida. Cancelando compra.")
+        print("La localidad ingresada no admite la opción de pago en efectivo.")
+        mostrar_menu()
 
 def procesar_factura():
     factura = Facturacion()
-
-    # Solicitar al usuario que ingrese la información necesaria
+    
     print("FACTURACIÓN DE PEDIDOS\n")
     print("Por favor complete los siguientes datos de su pedido")
     factura.nombre = input("Ingrese su nombre: ")
@@ -198,13 +233,13 @@ def procesar_factura():
 
     if opcion == 1:
         factura.metodo_pago = "Efectivo"
+        pago_en_efectivo(factura.localidad)        
     elif opcion == 2:
         factura.metodo_pago = "Transferencia"
     elif opcion == 3:
         factura.metodo_pago = "Tarjeta de Débito"    
     elif opcion == 4:
-        factura.metodo_pago = "Tarjeta de Crédito"
-        #factura.cuotas = opcion - 2   La opción 3 corresponde a 3 cuotas y la 4 a 6 cuotas.
+        factura.metodo_pago = "Tarjeta de Crédito"        
     else:
         print("Opción inválida. Por favor, seleccione una opción válida.")
 
@@ -212,9 +247,10 @@ def procesar_factura():
 
     try:
         precio_total = factura.calcular_precio_total(precio_producto)
-        print(f"El precio total de su pedido es: {precio_total}. Tenga en cuenta que el pago con tarjeta de crédito aplica recargos.")
+        print(f"El precio total de su pedido es: {precio_total}.")
 
         if factura.metodo_pago == "Tarjeta de Crédito":
+            print("Tenga en cuenta que el pago con tarjeta de crédito aplica recargos.")
             cuotas = solicitar_cuotas()
             factura.cuotas = cuotas
             precio_con_cuotas = factura.calcular_precio_total(precio_total)
@@ -223,12 +259,12 @@ def procesar_factura():
             confirmacion = input("¿Desea continuar con el pago? (s/n): ")
            
             if confirmacion.lower() == "s":
-                numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion = solicitar_datos_tarjeta()
-
+                numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion, tarjeta_elegida = solicitar_datos_tarjeta()
                 numero_tarjeta = factura.validar_numero_tarjeta(numero_tarjeta)
                 nombre_titular = factura.validar_nombre_titular(nombre_titular)
                 vencimiento = factura.validar_vencimiento(vencimiento)
-                codigo_verificacion = factura.validar_codigo_verificacion(codigo_verificacion)                
+                codigo_verificacion = factura.validar_codigo_verificacion(codigo_verificacion)
+                tarjeta_elegida = tarjeta_elegida
 
                 print("Datos de tarjeta validados.")
                 confirmacion = input("¿Desea continuar con el pago? (s/n): ")
@@ -243,3 +279,9 @@ def procesar_factura():
 
 if __name__ == "__main__":
     procesar_factura()
+
+
+
+
+
+
--- backend/pruebas.py
+++ backend/pruebas.py
@@ -1,76 +1,34 @@
-"""
-import re
-from datetime import datetime
 
-def validar_vencimiento(vencimiento):
+
+
+def solicitar_datos_tarjeta():
     while True:
-        if not re.match(r"^[0-9/]+$", vencimiento):
-            vencimiento = input("El formato no es válido. Por favor, ingrese solo 2 números para el mes, seguido de '/', y luego 2 números para el año: ")
-        elif not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", vencimiento):
-            vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+        print("\nOpciones de tarjetas:")
+        print("1. VISA")
+        print("2. Mastercard")
+        print("3. Naranja")
+        opcion_tarjeta = input("Elija una opción ingresando el N°: ")
+
+        if opcion_tarjeta in ["1", "2", "3"]:
+            tarjeta_elegida = None
+            if opcion_tarjeta == "1":
+                tarjeta_elegida = "VISA"
+            elif opcion_tarjeta == "2":
+                tarjeta_elegida = "Mastercard"
+            elif opcion_tarjeta == "3":
+                tarjeta_elegida = "Naranja"
+
+            numero_tarjeta = input("Ingrese el número de la tarjeta: ")
+            nombre_titular = input("Ingrese el nombre del titular de la tarjeta: ")
+            vencimiento = input("Ingrese el vencimiento de la tarjeta (MM/AA): ")
+            codigo_verificacion = input("Ingrese el código de verificación: ")
+
+            # Validaciones
+            numero_tarjeta = validar_numero_tarjeta(numero_tarjeta)
+            nombre_titular = validar_nombre_titular(nombre_titular)
+            vencimiento = validar_vencimiento(vencimiento)
+            codigo_verificacion = validar_codigo_verificacion(codigo_verificacion)
+
+            return numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion, tarjeta_elegida
         else:
-            mes, anio = vencimiento.split('/')
-
-            if not (mes.isdigit() and anio.isdigit()):
-                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            mes, anio = int(mes), int(anio)
-
-            if not (1 <= mes <= 12 and 0 <= anio <= 99):
-                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            # Obtener la fecha actual
-            fecha_actual = datetime.now()
-            if fecha_actual.year > anio or (fecha_actual.year == anio and fecha_actual.month > mes):
-                vencimiento = input("La fecha ingresada está vencida. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            if anio < 23 or (anio == 23 and mes < fecha_actual.month):
-                vencimiento = input("La fecha ingresada está vencida. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            return mes, anio
-
-vencimiento = input("Ingrese el vencimiento de la tarjeta (MM/AA): ")
-mes, anio = validar_vencimiento(vencimiento)
-print(f"Mes: {mes}, Año: {anio}")
-
-"""
-
-import re
-from datetime import datetime
-
-def validar_vencimiento(vencimiento):
-    while True:
-        if not re.match(r"^[0-9/]+$", vencimiento):
-            vencimiento = input("El formato no es válido. Por favor, ingrese solo 2 números para el mes, seguido de '/', y luego 2 números para el año: ")
-        elif not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", vencimiento):
-            vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
-        else:
-            mes, anio = vencimiento.split('/')
-
-            if not (mes.isdigit() and anio.isdigit()):
-                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            mes, anio = int(mes), int(anio)
-
-            if not (1 <= mes <= 12 and 0 <= anio <= 99):
-                vencimiento = input("El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            # Obtener la fecha actual
-            fecha_actual = datetime.now()
-            fecha_comparar = datetime(fecha_actual.year % 100, fecha_actual.month, 1)
-
-            if fecha_comparar > datetime(anio, mes, 1):
-                vencimiento = input("La fecha ingresada está vencida. Ingrese nuevamente (MM/AA): ")
-                continue
-
-            return mes, anio
-
-vencimiento = input("Ingrese el vencimiento de la tarjeta (MM/AA): ")
-mes, anio = validar_vencimiento(vencimiento)
-print(f"Mes: {mes}, Año: {anio}")
+            print("Opción de tarjeta inválida. Por favor, seleccione una opción válida.")
