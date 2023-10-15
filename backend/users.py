13fba95c9bfc5d2e257ae8af430748eefd0c0ebf
Vargas-Veronica <vargas.veronica55@gmail.com>
2023-10-14 21:54:43 -0300

    crud de usuarios con validaciones

--- backend/users.py
+++ backend/users.py
@@ -126,13 +126,6 @@ def delete_user(connection, user_id):
         print(f"Error al eliminar el usuario: {e}")
         
         
-def print_user(user):
-    print("\nDetalles del usuario:")
-    print(f"ID: {user.get_id_users()}")
-    print(f"Nombre: {user.get_first_name()} {user.get_last_name()}")
-    print(f"Email: {user.get_email()}")
-    print(f"Dirección: {user.get_address()}")
-    print(f"Imagen: {user.get_image()}")
 
 def read_user_by_id(connection, user_id):
     try:
@@ -150,9 +143,30 @@ def read_user_by_id(connection, user_id):
         print(f"Error al leer usuario por ID: {e}")
         return None
 
+def is_valid_string(value):
+    return bool(value.strip())
+
+def is_valid_email(value):
+    return "@" in value
+
+def is_valid_password(value):
+    return len(value) >= 6
+
+def is_valid_role(value):
+    return value.lower() in ["admin", "user"]
+
+def print_user(user):
+    print("\nDetalles del usuario:")
+    print(f"ID: {user.id_users}")
+    print(f"Nombre: {user.first_name} {user.last_name}")
+    print(f"Correo electrónico: {user.email}")
+    print(f"Dirección: {user.address}")
+    print(f"Imagen: {user.image}")
+
+
 def manage_users(connection):
     while True:
-        print("\nMenú de Usuarios:")
+        print("\nMenú:")
         print("1. Crear usuario")
         print("2. Leer usuarios")
         print("3. Actualizar usuario")
@@ -162,58 +176,103 @@ def manage_users(connection):
         choice = input("Selecciona una opción: ")
 
         if choice == "1":
-            first_name = input("Nombre: ")
+            first_name = input("Primer nombre: ")
             last_name = input("Apellido: ")
-            email = input("Email: ")
-            password = input("Contraseña: ")
+            while not is_valid_string(first_name) or not is_valid_string(last_name):
+                print("El nombre y el apellido no pueden estar en blanco.")
+                first_name = input("Primer nombre: ")
+                last_name = input("Apellido: ")
+
+            email = input("Correo electrónico: ")
+            while not is_valid_email(email):
+                print("El correo electrónico no es válido.")
+                email = input("Correo electrónico: ")
+
+            password = input("Contraseña (mínimo 6 caracteres): ")
+            while not is_valid_password(password):
+                print("La contraseña debe tener al menos 6 caracteres.")
+                password = input("Contraseña (mínimo 6 caracteres): ")
+
             address = input("Dirección: ")
-            image = input("URL de la imagen de perfil: ")
+            while not is_valid_string(address):
+                print("La dirección no puede estar en blanco.")
+                address = input("Dirección: ")
+
+            image = input("Imagen: ")
+            while not is_valid_string(image):
+                print("La imagen no puede estar en blanco.")
+                image = input("Imagen: ")
 
             new_user = User(None, first_name, last_name, email, password, address, image)
             user_id = create_user(connection, new_user)
             if user_id:
                 print(f"Usuario creado con ID: {user_id}")
+            else:
+                print("Error al crear el usuario")
 
         elif choice == "2":
             users = read_all_users(connection)
             if users:
-                print("\nListado de Usuarios:")
+                print("\nListado de usuarios:")
                 for user in users:
                     print_user(user)
+            else:
+                print("No hay usuarios registrados.")
 
         elif choice == "3":
-            user_id = int(input("ID del usuario a actualizar: "))
-            user_to_update = read_user_by_id(connection, user_id)
-            if user_to_update:
-                print(f"Usuario a actualizar:")
-                print_user(user_to_update)
-                first_name = input("Nuevo nombre (dejar en blanco para mantener el mismo): ")
-                last_name = input("Nuevo apellido (dejar en blanco para mantener el mismo): ")
-                email = input("Nuevo email (dejar en blanco para mantener el mismo): ")
-                password = input("Nueva contraseña (dejar en blanco para mantener la misma): ")
-                address = input("Nueva dirección (dejar en blanco para mantener la misma): ")
-                image = input("Nueva URL de imagen de perfil (dejar en blanco para mantener la misma): ")
+            user_id = input("ID del usuario a actualizar: ")
+            if user_id.isdigit():
+                user_id = int(user_id)
+                user_to_update = read_user_by_id(connection, user_id)
+                if user_to_update:
+                    print(f"Usuario a actualizar:")
+                    print_user(user_to_update)
 
-                if first_name:
-                    user_to_update.first_name = first_name
-                if last_name:
-                    user_to_update.last_name = last_name
-                if email:
-                    user_to_update.email = email
-                if password:
-                    user_to_update.password = password
-                if address:
-                    user_to_update.address = address
-                if image:
-                    user_to_update.image = image
+                    first_name = input(f"Nuevo primer nombre (deja en blanco para mantener el mismo: {user_to_update.first_name}): ")
+                    last_name = input(f"Nuevo apellido (deja en blanco para mantener el mismo: {user_to_update.last_name}): ")
+                    email = input(f"Nuevo correo electrónico (deja en blanco para mantener el mismo: {user_to_update.email}): ")
+                    password = input("Nueva contraseña (deja en blanco para mantener la misma): ")
 
-                update_user(connection, user_to_update)
-                print("Usuario actualizado")
+                    address = input(f"Nueva dirección (deja en blanco para mantener el mismo: {user_to_update.address}): ")
+                    while not is_valid_string(address):
+                        print("La dirección no puede estar en blanco.")
+                        address = input("Dirección: ")
+
+                    image = input(f"Nueva imagen (deja en blanco para mantener la misma: {user_to_update.image}): ")
+                    while not is_valid_string(image):
+                        print("La imagen no puede estar en blanco.")
+                        image = input("Imagen: ")
+
+                    if is_valid_string(first_name):
+                        user_to_update.first_name = first_name
+                    if is_valid_string(last_name):
+                        user_to_update.last_name = last_name
+                    if is_valid_email(email):
+                        user_to_update.email = email
+                    if is_valid_password(password):
+                        user_to_update.password = password
+                    if is_valid_string(address):
+                        user_to_update.address = address
+                    if is_valid_string(image):
+                        user_to_update.image = image
+
+                    update_user(connection, user_to_update)
+                    print("Usuario actualizado")
+
+                else:
+                    print("Usuario no encontrado.")
+
+            else:
+                print("ID de usuario no válido.")
 
         elif choice == "4":
-            user_id = int(input("ID del usuario a eliminar: "))
-            delete_user(connection, user_id)
-            print("Usuario eliminado")
+            user_id = input("ID del usuario a eliminar: ")
+            if user_id.isdigit():
+                user_id = int(user_id)
+                delete_user(connection, user_id)
+                print("Usuario eliminado")
+            else:
+                print("ID de usuario no válido.")
 
         elif choice == "5":
             break
