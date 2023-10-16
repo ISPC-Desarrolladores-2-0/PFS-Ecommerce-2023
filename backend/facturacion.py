60d6d1671382d8fae59991dbcff50b8126577f4d
Emilce Robles <emi.nrobles@gmail.com>
2023-10-16 20:50:21 -0300

    se complementan metodos de pago para facturacion

--- backend/facturacion.py
+++ backend/facturacion.py
@@ -11,7 +11,7 @@ class Facturacion:
     direccion = ""
     numero_calle = ""
     localidad = ""
-    metodo_pago = ""
+    payment_method = ""
     cuotas = 1
     telefono = ""
 
@@ -24,7 +24,7 @@ class Facturacion:
         self.direccion = ""
         self.numero_calle = ""
         self.localidad = ""
-        self.metodo_pago = ""
+        self.payment_method = ""
         self.cuotas = 1
         self.telefono = ""
 
@@ -46,8 +46,8 @@ class Facturacion:
     def get_localidad(self):
         return self.localidad
     
-    def get_metodo_pago(self):
-        return self.metodo_pago
+    def get_payment_method(self):
+        return self.payment_method
     
     def get_id_facturacion(self):
         return self.id_facturacion
@@ -55,7 +55,7 @@ class Facturacion:
     def get_id_cliente(self):
         return self.id_cliente
     
-    def get_telefono(self, telefono):
+    def get_telefono(self):
         return self.telefono
     
     def set_name(self, nombre):
@@ -76,8 +76,8 @@ class Facturacion:
     def set_localidad(self, localidad):
         self.localidad = localidad
     
-    def set_metodo_pago(self, metodo_pago):
-        self.metodo_pago = metodo_pago
+    def set_payment_method(self, payment_method):
+        self.payment_method = payment_method
     
     def set_id_facturacion(self, id_facturacion):
         self.id_facturacion = id_facturacion
@@ -88,127 +88,127 @@ class Facturacion:
     def set_telefono(self, telefono):
         self.telefono = telefono
         
-    def calcular_precio_total(self, precio_producto):
-        if self.metodo_pago == "Efectivo" or self.metodo_pago == "Transferencia":
-            return round(precio_producto, 2)
-        elif self.metodo_pago == "Tarjeta de Débito":
-            return round(precio_producto, 2)
-        elif self.metodo_pago == "Tarjeta de Crédito":
+    def calculate_total_price(self, product_price):
+        if self.payment_method == "Efectivo" or self.payment_method == "Transferencia":
+            return round(product_price, 2)
+        elif self.payment_method == "Tarjeta de Débito":
+            return round(product_price, 2)
+        elif self.payment_method == "Tarjeta de Crédito":
             if self.cuotas == 1:
-                return round(precio_producto * 1.05, 2)
+                return round(product_price * 1.05, 2)
             elif self.cuotas == 3:
-                return round(precio_producto * 1.15, 2)
+                return round(product_price * 1.15, 2)
             elif self.cuotas == 6:
-                return round(precio_producto * 1.30, 2)
+                return round(product_price * 1.30, 2)
             else:
                 raise ValueError("El número de cuotas no es válido")
         else:
             raise ValueError("Método de pago no válido")
     
-    def validar_numero_tarjeta(self, numero_tarjeta):
+    def validate_credit_card_number(self, numero_tarjeta):
         while not numero_tarjeta.isdigit() or len(numero_tarjeta) != 16:
             numero_tarjeta = input("➤El número de tarjeta no es válido. Ingrese nuevamente: ")
         return numero_tarjeta
     
     
-    def validar_nombre_titular(self, nombre_titular):
+    def validate_cardholder_name(self, nombre_titular):
         while len(nombre_titular.split()) != 2:
             nombre_titular = input("➤El nombre del titular debe tener nombre y apellido. Ingrese nuevamente: ")
         return nombre_titular
     
     @staticmethod
-    def validar_vencimiento(vencimiento):
+    def validate_expiration(expiration):
         while True:
-            if not re.match(r"^[0-9/]+$", vencimiento):
-                vencimiento = input("➤El formato del vencimiento no es válido. Por favor, ingrese solo 2 números para el mes, seguido de '/', y luego 2 números para el año: ")
-            elif not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", vencimiento):
-                vencimiento = input("➤El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+            if not re.match(r"^[0-9/]+$", expiration):
+                expiration = input("➤El formato del vencimiento no es válido. Por favor, ingrese solo 2 números para el mes, seguido de '/', y luego 2 números para el año: ")
+            elif not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", expiration):
+                expiration = input("➤El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
             else:
-                mes, anio = vencimiento.split('/')
+                mes, anio = expiration.split('/')
 
                 if not (mes.isdigit() and anio.isdigit()):
-                    vencimiento = input("➤El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+                    expiration = input("➤El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
                     continue
 
                 mes, anio = int(mes), int(anio)
 
                 if not (1 <= mes <= 12 and 0 <= anio <= 99):
-                    vencimiento = input("➤El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
+                    expiration = input("➤El formato de vencimiento no es válido. Ingrese nuevamente (MM/AA): ")
                     continue
 
-                fecha_actual = datetime.now()
-                fecha_comparar = datetime(fecha_actual.year % 100, fecha_actual.month, 1)
+                actual_date = datetime.now()
+                compare_date = datetime(actual_date.year % 100, actual_date.month, 1)
 
-                if fecha_comparar > datetime(anio, mes, 1):
-                    vencimiento = input("➤La fecha ingresada está vencida. Ingrese nuevamente (MM/AA): ")
+                if compare_date> datetime(anio, mes, 1):
+                    expiration = input("➤La fecha ingresada está vencida. Ingrese nuevamente (MM/AA): ")
                     continue               
-            return vencimiento       
+            return expiration       
                 
-    def validar_codigo_verificacion(self, codigo_verificacion):
-        while not codigo_verificacion.isdigit() or len(codigo_verificacion) != 3:
-            codigo_verificacion = input("➤El código de verificación no es válido. Ingrese nuevamente: ")
-        return codigo_verificacion
+    def validate_verification_code(self, verification_code):
+        while not verification_code.isdigit() or len(verification_code) != 3:
+            verification_code = input("➤El código de verificación no es válido. Ingrese nuevamente: ")
+        return verification_code
 
 
-def mostrar_menu():
+def display_menu():
     while True:
         print("\nMétodos de Pago:\n")
         print("1. Efectivo")
         print("2. Transferencia")
         print("3. Tarjeta de Débito")
         print("4. Tarjeta de Crédito")
-        opcion = input("Elija una opción ingresando el N°: ")
+        option = input("Elija una opción ingresando el N°: ")
 
-        if opcion in ["1", "2", "3", "4"]:
-            return int(opcion)
+        if option in ["1", "2", "3", "4"]:
+            return int(option)
         else:
             print("Opción inválida. Por favor, seleccione una opción válida.")
 
 
-def solicitar_cuotas():
+def request_credit_card_installments():
     while True:
         print("\nOpciones de cuotas:")
         print("1 cuota (5% de recargo)")
         print("3 cuotas (15% de recargo)")
         print("6 cuotas (30% de recargo)")
-        opcion_cuotas = input("➤Elija una opción ingresando el N° de cuotas: ")
+        option_installments = input("➤Elija una opción ingresando el N° de cuotas: ")
 
-        if opcion_cuotas in ["1", "3", "6"]:
-            return int(opcion_cuotas)
+        if option_installments in ["1", "3", "6"]:
+            return int(option_installments)
         else:
             print("El número de cuotas no es válido. Por favor, seleccione una opción válida.")
 
 
-def solicitar_datos_tarjeta():
+def request_card_information():
     while True:
         print("\nOpciones de tarjetas:")
         print("1. VISA")
         print("2. Mastercard")
         print("3. Naranja")
-        opcion_tarjeta = input("➤Elija una opción ingresando el N°: ")
+        option_card = input("➤Elija una opción ingresando el N°: ")
 
-        if opcion_tarjeta in ["1", "2", "3"]:
-            tarjeta_elegida = None
-            if opcion_tarjeta == "1":
-                tarjeta_elegida = "VISA"
-            elif opcion_tarjeta == "2":
-                tarjeta_elegida = "Mastercard"
-            elif opcion_tarjeta == "3":
-                tarjeta_elegida = "Naranja"
+        if option_card in ["1", "2", "3"]:
+            card_select = None
+            if option_card == "1":
+                card_select = "VISA"
+            elif option_card == "2":
+                card_select = "Mastercard"
+            elif option_card == "3":
+                card_select = "Naranja"
 
             numero_tarjeta = input("➤Ingrese el número de la tarjeta: ")
             nombre_titular = input("➤Ingrese el nombre del titular de la tarjeta: ")
-            vencimiento = input("➤Ingrese el vencimiento de la tarjeta (MM/AA): ")
-            codigo_verificacion = input("➤Ingrese el código de verificación: ")
-            return numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion, tarjeta_elegida
+            expiration = input("➤Ingrese el vencimiento de la tarjeta (MM/AA): ")
+            verification_code = input("➤Ingrese el código de verificación: ")
+            return numero_tarjeta, nombre_titular, expiration, verification_code, card_select
         else:
             print("Opción de tarjeta inválida. Por favor, seleccione una opción válida.")
 
-def validar_localidad_cba(localidad):
+def validate_locality_cba(localidad):
     return localidad.lower() in ['cordoba capital', 'córdoba capital']
 
-def pago_en_efectivo(localidad):    
-    if validar_localidad_cba(localidad):
+def cash_payment(localidad):    
+    if validate_locality_cba(localidad):
         print("\n\Opciones para pago en efectivo:")
         print("1. Retirar en el local")
         print("2. Abonar al recibir en domicilio")
@@ -232,7 +232,7 @@ def transfer_pay():
     print("Por favor realiza el envío de la transferencia al alias: planet.superheroes \nBanco de Córdoba\nUna vez completado envianos el comprobante por mail a: pagos@planetsuper.com.ar")       
     print("◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈◇◈\n")
  
-def procesar_factura():
+def process_billing():
     factura = Facturacion()
     
     print("\n\tFACTURACIÓN DE PEDIDOS\n")
@@ -246,43 +246,43 @@ def procesar_factura():
     factura.telefono = input("Ingrese su número de teléfono: ")
     
     while True:
-        opcion = mostrar_menu()
+        option = display_menu()
         
-        if opcion == 1:
-            factura.metodo_pago = "Efectivo"
-            pago_en_efectivo(factura.localidad)        
-        elif opcion == 2:
-            factura.metodo_pago = "Transferencia"
+        if option == 1:
+            factura.payment_method = "Efectivo"
+            cash_payment(factura.localidad)        
+        elif option == 2:
+            factura.payment_method = "Transferencia"
             transfer_pay()
-        elif opcion == 3:
-            factura.metodo_pago = "Tarjeta de Débito"    
-        elif opcion == 4:
-            factura.metodo_pago = "Tarjeta de Crédito"        
+        elif option == 3:
+            factura.payment_method = "Tarjeta de Débito"    
+        elif option == 4:
+            factura.payment_method = "Tarjeta de Crédito"        
         else:
             print("Opción inválida. Por favor, seleccione una opción válida.")
 
-        precio_producto = 9800.00  # Precio ficticio, tendria que ser el que se consulta a la BDD
+        product_price = 9800.00  # Precio ficticio, tendria que ser el que se consulta a la BDD
 
         try:
-            precio_total = factura.calcular_precio_total(precio_producto)
-            print(f"El precio total de su pedido es: {precio_total}.")
+            total_price = factura.calculate_total_price(product_price)
+            print(f"El precio total de su pedido es: {total_price}.")
 
-            if factura.metodo_pago == "Tarjeta de Crédito":
+            if factura.payment_method == "Tarjeta de Crédito":
                 print("Tenga en cuenta que el pago con tarjeta de crédito aplica recargos.")
-                cuotas = solicitar_cuotas()
+                cuotas = request_credit_card_installments()
                 factura.cuotas = cuotas
-                precio_con_cuotas = factura.calcular_precio_total(precio_total)
+                precio_con_cuotas = factura.calculate_total_price(total_price)
                 print(f"El precio con cuotas es: {precio_con_cuotas}")
 
                 confirmacion = input("➤¿Desea continuar con el pago? (s/n): ")
            
                 if confirmacion.lower() == "s":
-                    numero_tarjeta, nombre_titular, vencimiento, codigo_verificacion, tarjeta_elegida = solicitar_datos_tarjeta()
-                    numero_tarjeta = factura.validar_numero_tarjeta(numero_tarjeta)
-                    nombre_titular = factura.validar_nombre_titular(nombre_titular)
-                    vencimiento = factura.validar_vencimiento(vencimiento)
-                    codigo_verificacion = factura.validar_codigo_verificacion(codigo_verificacion)
-                    tarjeta_elegida = tarjeta_elegida
+                    numero_tarjeta, nombre_titular, expiration, verification_code, card_select = request_card_information()
+                    numero_tarjeta = factura.validate_credit_card_number(numero_tarjeta)
+                    nombre_titular = factura.validate_cardholder_name(nombre_titular)
+                    expiration = factura.validate_expiration(expiration)
+                    verification_code = factura.validate_verification_code(verification_code)
+                    card_select = card_select
 
                     print("Datos de tarjeta validados.")
                     confirmacion = input("¿Desea continuar con el pago? (s/n): ")
@@ -299,7 +299,7 @@ def procesar_factura():
             break
 
 if __name__ == "__main__":
-    procesar_factura()
+    process_billing()
 
 
 
